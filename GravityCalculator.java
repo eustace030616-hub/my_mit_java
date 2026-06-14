1a17961a68769c7074b05fd2bde80da2fce1ce72
eustace030616-hub <penghaoxi030616@gmail.com>
2026-06-14 22:12:14 +0800

    lecture1

---
+++ GravityCalculator.java
@@ -0,0 +1,13 @@
+public class GravityCalculator {
+    public static double distanceAfterTime(double t, double v1) {
+        double a = 9.8;
+        double distance = 0.5 * a * t * t + v1 * t;
+        return distance;
+    }
+    public static void main(String[] args) {
+        double t = 2; // set time of the free fall unit seconds
+        double v1 = 0; // set inital speed of the free fall unit meters per second
+
+        System.out.println(distanceAfterTime(t, v1));
+    }
+}
